cd0b1e0aa4a1b9909e6424ff3ad88bcc9b9de151
fatigba <fatigba72@gmail.com>
2018-02-28 21:31:24 +0000

    test(entités): insertion et suppresion dans la base de données. Avec le code de base, j'ai ajouté de quoi faire des opérations sur les comptes et lire ces opérations. Ensuite je supprime jordy et un de ses compte. Ça fonctionne. Persistence et lecture des groupes. La persistence et le lecture des groupes ne sembles pas fonctionner.

--- test/test_entities.cpp
+++ test/test_entities.cpp
@@ -15,6 +15,11 @@
  *				INSERTION DE GROUPE
  *				Deux groupes seront créer {groupe1, groupe2}.
  *        sam et edem-kouakou appartiendront chacun aux deux groupes.
+ *        INSERTION DES OPÉRATIONS
+ *        sam fera un virement d'un compte de jordy vers un compte de ablavi.
+ *        edem-kouakou fera une opération sur le compte de ablavi.
+ *        SUPRESSION DE COMPTE
+ *        Un compte de jordy sera supprimé. Sur ce compte, une opération est déjà faite.
  *
  */
 
@@ -48,6 +53,30 @@ std::ostream &operator<<(std::ostream &out, const Account &account)
     return out ;
 }
 
+// Lecture d'une opération
+std::ostream &operator<<(std::ostream &out, const Operation &operation)
+{
+    out <<"ID: "<< operation.getId() <<"\n" ;
+    out <<"Montant: "<< operation.getMontant() <<"\n" ;
+    out <<"Date: "<< operation.getDate() <<"\n" ;
+    out <<"Account: "<< operation.getAccountSource() <<"\n" ;
+    out <<"Employee: "<< operation.getEmployee() <<"\n" ;
+    return out ;
+}
+
+// Lecture d'un groupe
+std::ostream &operator<<(std::ostream &out, const Group &group)
+{
+    out << "ID: "<< group.getId()<<"\n" ;
+    out << "Nom: "<<group.getName()<<"\n" ;
+    out << "Membres: \n";
+    for(auto member : group.getMembers())
+    {
+        out << *member ;
+    }
+    return out ;
+}
+
 int main(void)
 {
     try
@@ -60,17 +89,22 @@ int main(void)
         long account1jojo_id, account2jojo_id, account3jojo_id ;
         long account1ablavi_id, account2ablavi_id ;
 
-/*        {
+        {
             //Persistence des Personnes
             // Les groupes
+            std::cout<< "Persistences des personnes et des groupes !\n" ;
             Group group1(0, "group1") ;
             Group group2(0, "group2") ;
             // Les employés
             Employee sam(0, "sam") ;
             Employee edem(0, "edem-kouakou") ;
+            sam.addGroup(group1) ;
+            sam.addGroup(group2) ;
+            edem.addGroup(group1) ;
+            edem.addGroup(group2) ;
             group1.push_back(sam) ;
             group1.push_back(edem) ;
-            group2.push_back(edem) ;
+            group2.push_back(sam) ;
             group2.push_back(edem) ;
             // Les clients
             Customer jordy(0, "jordy") ;
@@ -86,11 +120,11 @@ int main(void)
             p_database->persist(group2) ;
             t_transaction.commit() ;
         }
-*/
 
         // Créations des comptes
-/*        {
+        {
             t_transaction.reset(p_database->begin()) ;
+            std::cout<< "Création des comptes !\n" ;
             // Lecture des clients
             std::shared_ptr<Customer> jordy(p_database->load<Customer>(jordy_id)) ;
             std::shared_ptr<Customer> ablavi(p_database->load<Customer>(ablavi_id)) ;
@@ -104,20 +138,21 @@ int main(void)
             CurrentAccount ca1_jojo(base_sam) ;
             CurrentAccount ca2_jojo(base_sam) ;
             CurrentAccount ca1_ablavi(base_edem) ;
+
             // Appartenance des comptes
             // Les comptes de jordy créer par sam
             jordy->push_back(sa1_jojo) ;
             jordy->push_back(ca1_jojo) ;
             jordy->push_back(ca2_jojo) ;
             sam->addAccount(sa1_jojo) ;
-            sam->addAccount(ca1_ablavi) ;
+            sam->addAccount(ca1_jojo) ;
             sam->addAccount(ca2_jojo) ;
             // Les comptes créer par edem pour ablavi
             ablavi->push_back(ca1_ablavi) ;
             edem->addAccount(ca1_ablavi) ;
             //* Persistence des comptes
-            p_database->persist(ca1_ablavi) ;
-            p_database->persist(ca1_jojo) ;
+            account1ablavi_id = p_database->persist(ca1_ablavi) ;
+            account1jojo_id = p_database->persist(ca1_jojo) ;
             p_database->persist(sa1_jojo) ;
             p_database->persist(ca2_jojo) ;
             // mise à jours des employées
@@ -126,12 +161,42 @@ int main(void)
             p_database->update<Customer>(*jordy) ;
             p_database->update<Customer>(*ablavi) ;
 
+            std::cout << "Insertion des opérations\n";
+            // L'opération de sam, sam fait une opération sur ca1
+
+            Operation sam_operation(TypeOperation::DEPOT) ;
+            sam_operation.setId(0) ;
+            sam_operation.setAccountSource(ca1_jojo) ;
+            sam_operation.setEmployee(*sam) ;
+            sam_operation.setDate("28-02-2018") ;
+            sam_operation.setMontant(850.95) ;
+            // Mise à jour du compte et de l'employé
+            ca1_jojo.addOperation(sam_operation) ;
+            sam->addOperation(sam_operation) ;
+            p_database->persist(sam_operation) ;
+            p_database->update<CurrentAccount>(ca1_jojo) ;
+            p_database->update<Employee>(*sam) ;
+
+            // L'opération de edem, edem fait une opération sur ca2
+            Operation edem_operation(TypeOperation::RETRAIT) ;
+            edem_operation.setId(0) ;
+            edem_operation.setAccountSource(ca1_ablavi) ;
+            edem_operation.setEmployee(*edem) ;
+            edem_operation.setDate("28-02-2018") ;
+            edem_operation.setMontant(850.95) ;
+            // Mise à jour du compte et de l'employé
+            ca1_ablavi.addOperation(edem_operation) ;
+            edem->addOperation(edem_operation) ;
+            p_database->persist(edem_operation) ;
+            p_database->update<CurrentAccount>(ca1_ablavi) ;
+            p_database->update<Employee>(*edem) ;
             t_transaction.commit() ;
         }
-*/
+
         // Lecture des Personnes
         {
             odb::session s ;
+            std::cout<< "Affichage des personnes !\n" ;
             t_transaction.reset(p_database->begin()) ;
             odb::result<Person> t_result(p_database->query<Person>()) ;
             for(auto it(t_result.begin()); it!= t_result.end(); it++)
@@ -141,16 +206,55 @@ int main(void)
             }
             t_transaction.commit() ;
         }
+        // Lesture des groupes
+        {
+            try
+            {
+                odb::session s ;
+                std::cout<< "\nAffichage des groupes !\n" ;
+                t_transaction.reset(p_database->begin()) ;
+                odb::result<Group> t_result(p_database->query<Group>()) ;
+                for(odb::result<Group>::iterator it(t_result.begin()); it!= t_result.end(); it++)
+                {
+                    std::shared_ptr<Group> g(it.load()) ;
+                    std::cout << *g ;
+                }
+                t_transaction.commit() ;
+            }
+            catch(const std::exception &e)
+            {
+                puts(e.what()) ;
+            }
+        }
         // Lecture des comptes
         {
             odb::session s ;
+            std::cout<< "\nAffichage des comptes !\n" ;
             t_transaction.reset(p_database->begin()) ;
-            odb::result<Account> t_result(p_database->query<Account>()) ;
-            for(odb::result<Account>::iterator it(t_result.begin()); it!= t_result.end(); it++)
+            odb::result<CurrentAccount> t_result(p_database->query<CurrentAccount>()) ;
+            for(odb::result<CurrentAccount>::iterator it(t_result.begin()); it!= t_result.end(); it++)
             {
-                std::shared_ptr<Account> a(it.load()) ;
+                std::shared_ptr<CurrentAccount> a(it.load()) ;
                 std::cout << *a ;
             }
+            t_transaction.commit() ;
+        }
+
+        // Lectures des Opération et suppression d'un compte avec opération et proproétaire
+        {
+            odb::session s ;
+            std::cout<< "Affichage des opérations !\n" ;
+            t_transaction.reset(p_database->begin()) ;
+            odb::result<Operation> t_result(p_database->query<Operation>()) ;
+            for(auto it = t_result.begin(); it != t_result.end(); it++)
+            {
+                std::shared_ptr<Operation> op(it.load()) ;
+                std::cout << *op ;
+            }
+            std::cout<< "Suppression de ID: " << account1jojo_id << " compte de jordy !\n" ;
+            p_database->erase<CurrentAccount>(account1jojo_id) ;
+            std::cout<< "Suppression de jojo" ;
+            p_database->erase<Customer>(jordy_id) ;
             t_transaction.commit() ;
         }
     }
