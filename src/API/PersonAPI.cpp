e126da5370abb95166f0db68cdf89955a0e87ce0
fatigba <fatigba72@gmail.com>
2018-03-02 11:52:39 +0000

    code(ajout de classes): PersonAPI. J'ai ajouté au projet les classes d'API pour faire des opérations dans la base de données sur les tables Customer, Person, et Employee.

---
+++ src/API/PersonAPI.cpp
@@ -0,0 +1,53 @@
+#include "PersonAPI.hpp"
+
+// PersonAPI
+PersonAPI* PersonAPI::p_singleton = nullptr ;
+
+PersonAPI::PersonAPI()
+{
+
+}
+
+PersonAPI* PersonAPI::getInstance()
+{
+	if(p_singleton == nullptr)
+	{
+		p_singleton = new PersonAPI ;
+	}
+	return p_singleton ;
+}
+
+// CustomerAPI
+CustomerAPI* CustomerAPI::p_singleton = nullptr ;
+
+CustomerAPI::CustomerAPI()
+{
+
+}
+
+CustomerAPI* CustomerAPI::getInstance()
+{
+	if(p_singleton == nullptr)
+	{
+		p_singleton = new CustomerAPI ;
+	}
+	return p_singleton ;
+}
+
+// EmployeeAPI
+EmployeeAPI* EmployeeAPI::p_singleton = nullptr ;
+
+EmployeeAPI::EmployeeAPI()
+{
+
+}
+
+EmployeeAPI* EmployeeAPI::getInstance()
+{
+	if(p_singleton == nullptr)
+	{
+		p_singleton = new EmployeeAPI ;
+	}
+	return p_singleton ;
+}
+
---
+++ src/API/PersonAPI.hpp
@@ -0,0 +1,66 @@
+#ifndef ENTITYAPI_HPP_INCLUDED
+#define ENTITYAPI_HPP_INCLUDED
+
+/*
+ * \file PersonAPI.hpp
+ * \author jordy fatigba fatigba@gmail.com
+ * \brief Ce fichier contient toute les classes
+ *				d'API pour faire des opérations dans la
+ *				base de données sur les tables Customer,
+ *				Person, et Employee.
+ */
+
+// Forward declaration
+// Les classes entités
+class Entity::Customer ;
+class Entity::Person ;
+class Entity::Employee ;
+
+#include <iostream>
+#include <exception>
+
+// La classe de connection à la base de données
+class Core::DBConnection ;
+
+namespace API
+{
+
+/**
+ * \class PersonAPI
+ * \brief Cette classe permet de faire des opérations
+ *        d'insertion de suppression de recherche dans
+ *		  la base de données avec les entitées
+ *		  Person, Customer et Employee.
+ */
+class PersonAPI
+{
+private:
+	static PersonAPI *p_singleton ;
+	PersonAPI() ;
+public:
+  static PersonAPI *getInstance() ;
+  ~PersonAPI() ;
+};
+
+class CustomerAPI
+{
+private:
+	static CustomerAPI *p_singleton ;
+	CustomerAPI() ;
+public:
+  static CustomerAPI *getInstance() ;
+  ~CustomerAPI() ;
+};
+
+class EmployeeAPI
+{
+private:
+	static EmployeeAPI *p_singleton ;
+	EmployeeAPI() ;
+public:
+  static EmployeeAPI *getInstance() ;
+  ~EmployeeAPI() ;
+};
+
+}
+#endif // ENTITYAPI_HPP_INCLUDED
