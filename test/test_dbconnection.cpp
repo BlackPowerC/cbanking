1e21f0cb2a8b8bd57a3139bdc44726e9f386d126
fatigba <fatigba72@gmail.com>
2018-03-02 16:34:37 +0000

    Test de la classe DBConnecion

---
+++ test/test_dbconnection.cpp
@@ -0,0 +1,12 @@
+//#include "../src/Core/DBConnection.hpp"
+
+//#include <plog/Log.h>
+//#include <plog/Appenders/ColorConsoleAppender.h>
+
+//int main()
+//{
+//	static plog::ColorConsoleAppender<plog::TxtFormatter> consoleAppender;
+//	plog::init(plog::verbose, &consoleAppender);
+//  DBConnection::getInstance() ;
+//  return 0 ;
+//}
