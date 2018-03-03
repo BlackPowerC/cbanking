64459b1f135d12719f175b67c773970bf02d5d55
fatigba <fatigba72@gmail.com>
2018-03-03 18:18:59 +0000

    test(persistence des personnes): test_personapi.cpp. J'ai créer un code de test pour la classe PersonAPI. Un fichier CMaLelists.txt est aussi créé.

---
+++ test/personAPI/test_personapi.cpp
@@ -0,0 +1,65 @@
+/**
+ * \file test_personapi.cpp
+ * \author fatigba jordy fatigba72@gmail.com
+ * \date 26-02-2018
+ * \brief Ce fichier va servir à effectuer un test de la
+ *        classe PersonAPI.
+ *        Toute les méthodes seront appelées.
+ */
+
+#include "../../src/Exception.hpp"
+#include "../../src/Entity/Entity.hpp"
+#include "../../src/API/PersonAPI.hpp"
+#include <iostream>
+#include <exception>
+
+
+using namespace Entity ;
+using namespace API ;
+
+// Lecture d'une personne
+std::ostream &operator<<(std::ostream &out, const Person &person)
+{
+    out << "ID: " << person.getId() << std::endl ;
+    out << "Nom: " << person.getName() << std::endl ;
+    return out ;
+}
+
+#include <plog/Log.h>
+#include <plog/Appenders/ColorConsoleAppender.h>
+
+int main()
+{
+    static plog::ColorConsoleAppender<plog::TxtFormatter> consoleAppender;
+    plog::init(plog::verbose, &consoleAppender);
+
+    PersonAPI *p_papi = PersonAPI::getInstance() ;
+
+    /* Persitence de 2 Customer et d'un Employee */
+    {
+        // Lecture de tout les customers
+        try
+        {
+            Customer dalida(0, "dalida") ;
+            Customer thierrno(0, "thierrno") ;
+            Customer nabine(0, "nabine") ;
+            p_papi->insert<Customer>(dalida) ;
+            p_papi->insert<Customer>(nabine) ;
+            p_papi->insert<Customer>(thierrno) ;
+            for(auto &c: p_papi->findAll<Customer>())
+            {
+              std::cout << *c ;
+            }
+        }
+        catch(const BaseException &ba)
+        {
+            LOG_ERROR << ba.what() ;
+        }
+        catch(const std::exception &e)
+        {
+            LOG_ERROR << e.what() ;
+        }
+    }
+
+    return 0 ;
+}
