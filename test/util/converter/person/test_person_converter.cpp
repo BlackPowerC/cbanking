12b34657b451ee501533e11017f35df17291f3e9
fatigba <fatigba72@gmail.com>
2018-03-07 11:11:39 +0000

    test(api de conversion): test de PersonConverter. Test de la classe PersonConverter. Un test simple est mis dans le fichier. J'ai aussi ajouter le fichier CMakeLists.txt

---
+++ test/util/converter/person/test_person_converter.cpp
@@ -0,0 +1,42 @@
+/**
+ * \author jordy fatigba
+ * \brief Fichier de test de la classe PersonConverter.
+ *
+ */
+
+//
+// Created by jordy on 06/03/18.
+//
+
+#include <gtest/gtest.h>
+#include "../../../../include/Util/Converter/PersonConverter.hpp"
+
+class Test: public ::testing::Test
+{
+protected:
+    virtual void TearDown()
+    {
+
+    }
+
+    virtual void SetUp()
+    {
+
+    }
+};
+
+#include <iostream>
+
+TEST_F(Test, person_api_test1)
+{
+    Entity::Person jordy(6, "jordy") ;
+    Util::PersonConverter pc ;
+    std::string expected("{\"id\":6, \"name\":\"jordy\"}") ;
+    ASSERT_TRUE(expected == pc.entityToJson(jordy)) ;
+}
+
+int main(int argc, char *argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv) ;
+    return RUN_ALL_TESTS() ;
+}
