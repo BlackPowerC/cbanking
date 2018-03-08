9b81939d719fe2fac41f9abb327980b72f582cc1
fatigba <fatigba72@gmail.com>
2018-03-08 11:21:47 +0000

    feature(serveur rest): ajout d'une classe de méthodes distantes. J'ai créer la classe RequestHandler. Cette classe va contenir toutes les méthodes distantes du service web.

---
+++ include/Rest/RequestHandler.hpp
@@ -0,0 +1,25 @@
+//
+// Created by jordy on 08/03/18.
+//
+
+#ifndef REQUESTHANDLER_HPP
+#define REQUESTHANDLER_HPP
+
+/**
+ * \namespace RestAPI
+ * \brief Ce namespace contient les classes mettant en oeuvre
+ *        le web service REST.
+ */
+namespace RestAPI
+{
+    /**
+     * \struct ResquestHandler
+     * \brief Cette structure contient les méthodes distantes du web service
+     */
+    struct RequestHandler
+    {
+
+    };
+}
+
+#endif //_REQUESTHANDLER_HPP
