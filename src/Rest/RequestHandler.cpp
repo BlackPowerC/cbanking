04db8d1f6f8681ece09e4549dab4e5ac73427d97
fatigba <fatigba72@gmail.com>
2018-03-11 20:31:17 +0000

    feature(serveur rest): implémentation de getAccountById. Implémentation de la méthode getAccountById. Le test manuel avec curl fonctionne bien.

--- src/Rest/RequestHandler.cpp
+++ src/Rest/RequestHandler.cpp
@@ -6,37 +6,106 @@
  */
 
 #include "../../include/Rest/RequestHandler.hpp"
+#include "../../include/Exception.hpp"
+// API de persistence
+#include "../../include/API/PersistenceAPI.hpp"
+#include "../../include/API/AccountAPI.hpp"
+
+#include "../../include/Util/Converter/Converters.hpp"
+
+#include <exception>
+#include <plog/Log.h>
+
+using namespace Entity ;
+using namespace API ;
+using namespace Util ;
 
 namespace RestAPI
 {
     void RequestHandler::getAccountById(const Rest::Request &request, Http::ResponseWriter response)
     {
-    	response.send(Http::Code::Ok, "Tout est OK", Http::Mime::MediaType::fromString("text/plain"));
+        /* Vérifions le paramettre */
+        long id ;
+        try
+        {
+            id = request.param(":id").as<int>() ;
+        }catch(const std::exception &e)
+        {
+            LOG_ERROR << e.what() ;
+            response.send(Http::Code::Not_Acceptable, e.what(), Http::Mime::MediaType::fromString("text/plain")) ;
+            return ;
+        }
+        // Pas d'erreur on continue
+        bool flag = true ;
+        std::string error ;
+        std::string json ;
+        std::shared_ptr<Account> account ;
+        {
+            try
+            {
+                /* voyons voir si le comptes est courant ou non */
+                account = AccountAPI::getInstance()->findById<CurrentAccount>(id) ;
+            }catch(const NotFound &nf)
+            {
+                flag = false ;
+                error = std::string(nf.what()) ;
+                LOG_ERROR << nf.what() ;
+            }
+        }
+        // Il n'y a pas de compte courant avec cette ID, on essaie
+        // avec un compte d'épargne.
+        if(!flag)
+        {
+            try
+            {
+                /* voyons voir si le compte est courant ou non */
+                account = AccountAPI::getInstance()->findById<SavingsAccount>(id) ;
+            }catch(const NotFound &nf)
+            {
+                LOG_ERROR << nf.what() ;
+                response.send(Http::Code::Not_Found, nf.what(), MIME(Text, Plain)) ;
+                return ;
+            }
+            // Il s'agit d'un compte d'épargne. On envoie une réponse.
+            json = SavingsAccountConverter().entityToJson(dynamic_cast<SavingsAccount*>(account.get()));
+            response.send(Http::Code::Ok, json, MIME(Application, Json)) ;
+            return ;
+        }
+        // Il s'agit d'un compte courant j'envoie la bonne réponse
+        json = CurrentAccountConverter().entityToJson(dynamic_cast<CurrentAccount*>(account.get()));
+        response.send(Http::Code::Ok, json, MIME(Application, Json)) ;
     }
+
     void RequestHandler::getAccountByCustomerId(const Rest::Request &request, Http::ResponseWriter response)
     {
     	response.send(Http::Code::Ok, "Tout est OK", Http::Mime::MediaType::fromString("text/plain"));
     }
+
     void RequestHandler::getAccountByEmployeeId(const Rest::Request &request, Http::ResponseWriter response)
     {
     	response.send(Http::Code::Ok, "Tout est OK", Http::Mime::MediaType::fromString("text/plain"));
     }
+
     void RequestHandler::getEmployeeById(const Rest::Request &request, Http::ResponseWriter response)
     {
     	response.send(Http::Code::Ok, "Tout est OK", Http::Mime::MediaType::fromString("text/plain"));
     }
+
     void RequestHandler::getAllEmployees(const Rest::Request &request, Http::ResponseWriter response)
     {
     	response.send(Http::Code::Ok, "Tout est OK", Http::Mime::MediaType::fromString("text/plain"));
     }
+
     void RequestHandler::getCustomerById(const Rest::Request &request, Http::ResponseWriter response)
     {
     	response.send(Http::Code::Ok, "Tout est OK", Http::Mime::MediaType::fromString("text/plain"));
     }
+
     void RequestHandler::getAllCustomers(const Rest::Request &request, Http::ResponseWriter response)
     {
     	response.send(Http::Code::Ok, "Tout est OK", Http::Mime::MediaType::fromString("text/plain"));
     }
+
     void RequestHandler::getCustomerByName(const Rest::Request &request, Http::ResponseWriter response)
     {
     	response.send(Http::Code::Ok, "Tout est OK", Http::Mime::MediaType::fromString("text/plain"));
@@ -47,10 +116,12 @@ namespace RestAPI
     {
     	response.send(Http::Code::Ok, "Tout est OK", Http::Mime::MediaType::fromString("text/plain"));
     }
+
     void RequestHandler::deleteAccount(const Rest::Request &request, Http::ResponseWriter response)
     {
     	response.send(Http::Code::Ok, "Tout est OK", Http::Mime::MediaType::fromString("text/plain"));
     }
+
     void RequestHandler::deleteOperation(const Rest::Request &request, Http::ResponseWriter response)
     {
     	response.send(Http::Code::Ok, "Tout est OK", Http::Mime::MediaType::fromString("text/plain"));
@@ -61,14 +132,17 @@ namespace RestAPI
     {
     	response.send(Http::Code::Ok, "Tout est OK", Http::Mime::MediaType::fromString("text/plain"));
     }
+
     void RequestHandler::addEmployee(const Rest::Request &request, Http::ResponseWriter response)
     {
     	response.send(Http::Code::Ok, "Tout est OK", Http::Mime::MediaType::fromString("text/plain"));
     }
+
     void RequestHandler::addAccount(const Rest::Request &request, Http::ResponseWriter response)
     {
     	response.send(Http::Code::Ok, "Tout est OK", Http::Mime::MediaType::fromString("text/plain"));
     }
+
     void RequestHandler::addOperation(const Rest::Request &request, Http::ResponseWriter response)
     {
     	response.send(Http::Code::Ok, "Tout est OK", Http::Mime::MediaType::fromString("text/plain"));
