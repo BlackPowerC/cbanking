977a10e8c5fe506f6c2922039e964fd0445eed48
fatigba <fatigba72@gmail.com>
2018-04-02 20:27:22 +0000

    feature(convertisseur): ajout de la classe OperationConverter.
    Donc c'est ça, je viens de créer la définition de la classe
    OperationConverter pour convertir les entités de la classe Operation.

---
+++ include/Util/Converter/OperationConverter.hpp
@@ -0,0 +1,34 @@
+//
+// Created by jordy on 02/04/18.
+//
+
+#ifndef OPERATIONCONVERTER_HPP
+#define OPERATIONCONVERTER_HPP
+
+#include "AccountConverter.hpp"
+#include "EmployeeConverter.hpp"
+
+namespace Util
+{
+    /**
+     * @class OperationConverter
+     * @brief Cette classe est la convertisseur de la classe Operation.
+     */
+    class OperationConverter: public Converter<Entity::Operation>
+    {
+    public:
+        /**
+         * \brief Cette fonction convertit une instance
+         *        de Operation en format JSON.
+         * @param entity
+         * @return Une chaine de caractère formaté en JSON.
+         */
+        virtual std::string entityToJson(const Entity::Operation &entity) = 0;
+
+        virtual std::string entityToJson(const Entity::Operation *entity) = 0;
+
+        virtual std::string entityToJson(const std::shared_ptr<Entity::Operation> &entity) = 0;
+    };
+}
+
+#endif //CBANKING_REST_API_OPERATIONCONVERTER_HPP
