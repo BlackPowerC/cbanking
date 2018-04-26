ad63954303ca495a93b2a743a59f83e35d284818
fatigba <fatigba72@gmail.com>
2018-04-26 11:14:55 +0000

    entrypoint(main): prise en compte des arguments de la ligne de commande.

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,22 @@
 // Mes includes
 #include "../include/Rest/Rest.hpp"
 #include "../include/Core/DBConnection.hpp"
+#include <cstdlib>
 
-int main()
+int main(char *argv[], int argc)
 {
 	static plog::ColorConsoleAppender<plog::TxtFormatter> consoleAppender;
 	plog::init(plog::verbose, &consoleAppender);
-    // Initialisation de la connexion à la db
-    DBConnection::getInstance() ;
-	RestAPI::RestServer r ;
+  // Initialisation de la connexion à la db
+  DBConnection::getInstance() ;
+	if(argc >= 3)
+	{
+		RestAPI::RestServer r(argv[1], std::atoi(argc[2])) ;
+	}
+	else
+	{
+		RestAPI::RestServer r ;
+	}
 	r.start() ;
 	r.stop() ;
 	std::cout << "Hello world !\n" ;
