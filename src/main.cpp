9eb317cc4fa4db9f949cde4e1d79526258b0c71f
fatigba <fatigba72@gmail.com>
2018-04-27 20:46:44 +0000

    Dockerfile et main.cpp
    Suppression des commandes ENV dans le dockerfile et ajout d'une CMD
    avec bash.
    Dans le main.cpp, prise en compte d"une seule variable de la cli.

--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,12 @@ int main(int argc, char **argv)
 {
 	static plog::ColorConsoleAppender<plog::TxtFormatter> consoleAppender;
 	plog::init(plog::verbose, &consoleAppender);
-  // Initialisation de la connexion à la db
-  DBConnection::getInstance() ;
-	if(argc >= 3)
+  	// Initialisation de la connexion à la db
+ 	DBConnection::getInstance() ;
+	if(argc >= 2)
 	{
         std::cout << argc ;
-		RestAPI::RestServer r(argv[1], std::atoi(argv[2])) ;
+		RestAPI::RestServer r(getenv("HOSTNAME"), std::atoi(argv[1])) ;
         r.start() ;
         r.stop() ;
 	}
